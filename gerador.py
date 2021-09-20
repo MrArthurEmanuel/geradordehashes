77d6df2aa7774f69079706dc20537304c2fdc01e
MrArthurEmanuel <91098280+MrArthurEmanuel@users.noreply.github.com>
2021-09-20 19:39:01 -0300

    Add files via upload

--- gerador.py
+++ gerador.py
@@ -1,5 +1,27 @@
 import hashlib
 
-resultado = hashlib.md5(b'Arthur')
+string = input('Digite o texto a ser gerado o HASH: ')
 
-print("A hash é: ", resultado.hexdigest())
+menu = int(input('''### MENU DOS HASHES ###
+--------------------------------------------
+1) MD5
+2) SHA1 
+3) SHA256
+4) SHA512
+Digite o numero do hash a ser gerado: '''))
+
+
+if menu == 1:
+    resultado = hashlib.md5(string.encode('utf-8'))
+    print("\nO Hash MD5 de",string, "é:", resultado.hexdigest())
+elif menu == 2:
+    resultado = hashlib.sha1(string.encode('utf-8'))
+    print("\nO Hash SHA1 de",string,"é:", resultado.hexdigest())
+elif menu == 3:
+    resultado = hashlib.sha256(string.encode('utf-8'))
+    print("\nO Hash SHA256 de",string,"é:", resultado.hexdigest())
+elif menu == 4:
+    resultado = hashlib.sha512(string.encode('utf-8'))
+    print("\nO Hash SHA512 de",string,"é:", resultado.hexdigest())
+else:
+    print('DEU ERRADO PORRA')
